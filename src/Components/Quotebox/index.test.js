081b7da50b85bd07ea831b849e8724fa5c15e388
Musab <musabsohail93@gmail.com>
2019-10-08 16:42:07 +0500

    QuoteAuthor in QuoteBox test - Failed

--- src/Components/Quotebox/index.test.js
+++ src/Components/Quotebox/index.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+
 import QuoteBox from './index'
-import QuoteText from '../Quotetext/';
+import QuoteAuthor from '../QuoteAuthor';
+import QuoteText from '../Quotetext';
 
 const component = shallow(<QuoteBox />);
         
@@ -16,12 +18,21 @@ describe('Render Quote Box Component', () => {
 })
 
 // Integration Test
-
 describe('Render QuoteText in QuoteBox', () => {
 
     it('Component with id "text" renders in QuoteBox without err!', () => {
-        const qutoeText = component.find(QuoteText)  
-        expect(qutoeText.length).toBe(1)
+        const innerDiv = component.find(QuoteText)  
+        expect(innerDiv.length).toBe(1)
+    })
+
+
+})
+
+describe('Render QuoteAuthor in QuoteBox', () => {
+
+    it('Component with id "author" renders in QuoteBox without err!', () => {
+        const innerDiv = component.find(QuoteAuthor)  
+        expect(innerDiv.length).toBe(1)
     })
 
 
